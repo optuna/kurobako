cdebdf5193455fb6106267168d6b2df99df9cc45
Takeru Ohta <phjgt308@gmail.com>
2019-10-01 00:19:38 +0900

    Remove rusty-machine from dependencies

--- src/plot_scatter.rs
+++ src/plot_scatter.rs
@@ -1,10 +1,6 @@
 use crate::record::Id;
 use crate::record::StudyRecord;
 use kurobako_core::{Error, Result};
-use rusty_machine::learning::gp;
-use rusty_machine::learning::SupModel;
-use rusty_machine::linalg::Matrix;
-use rusty_machine::linalg::Vector;
 use std::collections::BTreeMap;
 use std::path::Path;
 use structopt::StructOpt;
@@ -28,19 +24,12 @@ pub struct PlotScatterOptions {
 
     #[structopt(long, default_value = "")]
     pub prefix: String,
-
-    #[structopt(long)]
-    pub gp: bool,
 }
 impl PlotScatterOptions {
     pub fn plot_problems<P: AsRef<Path>>(&self, studies: &[StudyRecord], dir: P) -> Result<()> {
         let datasets = PlotDatasets::new(studies);
         for (i, (key, dataset)) in datasets.datasets.iter().enumerate() {
-            track!(dataset.plot(
-                dir.as_ref().join(format!("{}{}.dat", self.prefix, i)),
-                &key,
-                self.gp
-            ))?;
+            track!(dataset.plot(dir.as_ref().join(format!("{}{}.dat", self.prefix, i)), &key,))?;
 
             let commands = self.make_gnuplot_commands(
                 &key,
@@ -89,13 +78,6 @@ impl PlotScatterOptions {
             input.as_ref().to_str().expect("TODO")
         );
 
-        if self.gp {
-            s += &format!(
-                ", {:?} index 1 u 2:1 with lines notitle",
-                input.as_ref().to_str().expect("TODO")
-            );
-        }
-
         s
     }
 }
@@ -132,7 +114,6 @@ impl<'a> PlotDataset<'a> {
         &self,
         path: P,
         &(problem, solver, param): &(Id<'a>, Id<'a>, usize),
-        gp: bool,
     ) -> Result<()> {
         use std::fs::File;
         use std::io::Write;
@@ -155,47 +136,6 @@ impl<'a> PlotDataset<'a> {
             }
         }
 
-        if gp {
-            writeln!(f, "\n")?;
-            for (p, v) in track!(self.predict(param))? {
-                writeln!(f, "{} {}", v, p)?;
-            }
-        }
         Ok(())
-    }
-
-    fn predict(&self, param: usize) -> Result<impl Iterator<Item = (f64, f64)>> {
-        let mut train_data = Vec::<f64>::new();
-        let mut target = Vec::<f64>::new();
-
-        for study in &self.studies {
-            for (_budget, trial) in study.complete_trials() {
-                let v = trial.evaluate.values[0].get(); // TODO: support multi-objective
-                let p = trial.ask.params[param].to_f64();
-                train_data.push(p);
-                target.push(v);
-            }
-        }
-
-        let mut gaussp = gp::GaussianProcess::default();
-        gaussp.noise = 1e-3f64;
-
-        let train_data = Matrix::new(train_data.len(), 1, train_data);
-        let target = Vector::new(target);
-        track_any_err!(gaussp.train(&train_data, &target))?;
-
-        let mut params = Vec::new();
-        let range = self.studies[0].problem.spec.params_domain[param].range();
-        let width = range.width();
-        for i in 0..100 {
-            let p = range.low + (width / 100.0) * i as f64;
-            params.push(p);
-        }
-        let predicted = track_any_err!(gaussp.predict(&Matrix::new(params.len(), 1, params)))?;
-        Ok(predicted
-            .into_vec()
-            .into_iter()
-            .enumerate()
-            .map(move |(i, v)| (range.low + (width / 100.0) * i as f64, v)))
     }
 }
