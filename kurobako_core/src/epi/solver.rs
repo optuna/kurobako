504d8dc454337b22b0aab906561f1cdaabbade31
Takeru Ohta <phjgt308@gmail.com>
2019-11-12 16:41:25 +0900

    Add `epi/solver/embedded_script` module

--- kurobako_core/src/epi/solver.rs
+++ kurobako_core/src/epi/solver.rs
@@ -1,9 +1,11 @@
-// pub use self::embedded_script::{EmbeddedScriptSolver, EmbeddedScriptSolverRecipe};
-// pub use self::external_program::{
-//     ExternalProgramSolver, ExternalProgramSolverRecipe, SolverMessage,
-// };
+pub use self::embedded_script::{
+    EmbeddedScriptSolver, EmbeddedScriptSolverFactory, EmbeddedScriptSolverRecipe,
+};
+pub use self::external_program::{
+    ExternalProgramSolver, ExternalProgramSolverFactory, ExternalProgramSolverRecipe,
+};
 pub use self::message::SolverMessage;
 
-// mod embedded_script;
+mod embedded_script;
 mod external_program;
 mod message;
--- kurobako_core/src/epi/solver/embedded_script.rs
+++ kurobako_core/src/epi/solver/embedded_script.rs
@@ -1,77 +1,78 @@
-use crate::epi::solver::{ExternalProgramSolver, ExternalProgramSolverRecipe};
+use crate::epi::solver::{
+    ExternalProgramSolver, ExternalProgramSolverFactory, ExternalProgramSolverRecipe,
+};
 use crate::problem::ProblemSpec;
-use crate::solver::{ObservedObs, Solver, SolverRecipe, SolverSpec, UnobservedObs};
-use crate::{Error, ErrorKind, Result};
-use rand::Rng;
+use crate::repository::Repository;
+use crate::rng::ArcRng;
+use crate::solver::{Solver, SolverFactory, SolverRecipe, SolverSpec};
+use crate::trial::{EvaluatedTrial, IdGen, UnevaluatedTrial};
+use crate::{Error, Result};
 use serde::{Deserialize, Serialize};
 use std::io::Write as _;
-use std::path::PathBuf;
 use structopt::StructOpt;
 use tempfile::{NamedTempFile, TempPath};
-use yamakan::observation::IdGen;
 
 #[derive(Debug, Clone, StructOpt, Serialize, Deserialize)]
 #[structopt(rename_all = "kebab-case")]
-#[serde(rename_all = "kebab-case")]
 pub struct EmbeddedScriptSolverRecipe {
     pub script: String,
-
     pub args: Vec<String>,
-
-    #[structopt(long)]
-    pub interpreter: Option<PathBuf>,
-
-    pub interpreter_args: Vec<String>,
 }
 impl SolverRecipe for EmbeddedScriptSolverRecipe {
-    type Solver = EmbeddedScriptSolver;
+    type Factory = EmbeddedScriptSolverFactory;
 
-    fn create_solver(&self, problem: ProblemSpec) -> Result<Self::Solver> {
+    fn create_factory(&self, repository: &mut Repository) -> Result<Self::Factory> {
         let mut temp = track!(NamedTempFile::new().map_err(Error::from))?;
         track!(write!(temp.as_file_mut(), "{}", self.script).map_err(Error::from))?;
         let temp = temp.into_temp_path();
 
         let mut args = Vec::new();
-        let path = if let Some(interpreter_path) = self.interpreter.clone() {
-            args.extend(self.interpreter_args.clone());
-            args.push(track_assert_some!(temp.to_str(), ErrorKind::InvalidInput).to_owned());
-            interpreter_path
-        } else {
-            #[cfg(unix)]
-            {
-                use std::fs;
-                use std::os::unix::fs::PermissionsExt as _;
+        #[cfg(unix)]
+        {
+            use std::fs;
+            use std::os::unix::fs::PermissionsExt as _;
 
-                track!(
-                    fs::set_permissions(&temp, fs::Permissions::from_mode(0o755))
-                        .map_err(Error::from)
-                )?;
-            }
-            temp.to_path_buf()
-        };
+            track!(
+                fs::set_permissions(&temp, fs::Permissions::from_mode(0o755)).map_err(Error::from)
+            )?;
+        }
+        let path = temp.to_path_buf();
         args.extend(self.args.clone());
 
         let eppr = ExternalProgramSolverRecipe { path, args };
-        let inner = track!(eppr.create_solver(problem))?;
-        Ok(EmbeddedScriptSolver { inner, temp })
+        let inner = track!(eppr.create_factory(repository))?;
+        Ok(EmbeddedScriptSolverFactory { inner, temp })
+    }
+}
+
+#[derive(Debug)]
+pub struct EmbeddedScriptSolverFactory {
+    inner: ExternalProgramSolverFactory,
+    temp: TempPath,
+}
+impl SolverFactory for EmbeddedScriptSolverFactory {
+    type Solver = EmbeddedScriptSolver;
+
+    fn specification(&self) -> Result<SolverSpec> {
+        track!(self.inner.specification())
+    }
+
+    fn create_solver(&self, rng: ArcRng, problem: &ProblemSpec) -> Result<Self::Solver> {
+        let inner = track!(self.inner.create_solver(rng, problem))?;
+        Ok(EmbeddedScriptSolver { inner })
     }
 }
 
 #[derive(Debug)]
 pub struct EmbeddedScriptSolver {
     inner: ExternalProgramSolver,
-    temp: TempPath,
 }
 impl Solver for EmbeddedScriptSolver {
-    fn specification(&self) -> SolverSpec {
-        self.inner.specification()
+    fn ask(&mut self, idg: &mut IdGen) -> Result<UnevaluatedTrial> {
+        track!(self.inner.ask(idg))
     }
 
-    fn ask<R: Rng, G: IdGen>(&mut self, rng: R, idg: G) -> Result<UnobservedObs> {
-        track!(self.inner.ask(rng, idg))
-    }
-
-    fn tell(&mut self, obs: ObservedObs) -> Result<()> {
-        track!(self.inner.tell(obs))
+    fn tell(&mut self, trial: EvaluatedTrial) -> Result<()> {
+        track!(self.inner.tell(trial))
     }
 }
