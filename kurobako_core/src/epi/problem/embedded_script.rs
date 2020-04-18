67ed335e0a79dbca2478fa422c8a56cbc65945be
Takeru Ohta <phjgt308@gmail.com>
2020-04-18 23:39:06 +0900

    Fix a bug when calculating the cache key of an external program

--- kurobako_core/src/epi/problem/embedded_script.rs
+++ kurobako_core/src/epi/problem/embedded_script.rs
@@ -7,10 +7,17 @@ use crate::registry::FactoryRegistry;
 use crate::rng::ArcRng;
 use crate::trial::{Params, Values};
 use crate::{Error, Result};
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::io::Write as _;
+use std::sync::Mutex;
 use structopt::StructOpt;
 use tempfile::{NamedTempFile, TempPath};
+
+lazy_static! {
+    static ref TEMP_FILES: Mutex<HashMap<String, TempPath>> = Mutex::new(HashMap::new());
+}
 
 /// Recipe for the problem implemented by an embedded script.
 #[derive(Debug, Clone, StructOpt, Serialize, Deserialize)]
@@ -26,26 +33,37 @@ impl ProblemRecipe for EmbeddedScriptProblemRecipe {
     type Factory = EmbeddedScriptProblemFactory;
 
     fn create_factory(&self, registry: &FactoryRegistry) -> Result<Self::Factory> {
-        let mut temp = track!(NamedTempFile::new().map_err(Error::from))?;
-        track!(write!(temp.as_file_mut(), "{}", self.script).map_err(Error::from))?;
-        let temp = temp.into_temp_path();
+        let path = {
+            let mut temp_files = track!(TEMP_FILES.lock().map_err(Error::from))?;
+            if let Some(path) = temp_files.get(&self.script).map(|p| p.to_path_buf()) {
+                path
+            } else {
+                let mut temp = track!(NamedTempFile::new().map_err(Error::from))?;
+                track!(write!(temp.as_file_mut(), "{}", self.script).map_err(Error::from))?;
+                let temp = temp.into_temp_path();
 
-        #[cfg(unix)]
-        {
-            use std::fs;
-            use std::os::unix::fs::PermissionsExt as _;
+                #[cfg(unix)]
+                {
+                    use std::fs;
+                    use std::os::unix::fs::PermissionsExt as _;
 
-            track!(
-                fs::set_permissions(&temp, fs::Permissions::from_mode(0o755)).map_err(Error::from)
-            )?;
-        }
+                    track!(
+                        fs::set_permissions(&temp, fs::Permissions::from_mode(0o755))
+                            .map_err(Error::from)
+                    )?;
+                }
 
-        let path = temp.to_path_buf();
+                let path = temp.to_path_buf();
+                temp_files.insert(self.script.clone(), temp);
+                path
+            }
+        };
+
         let args = self.args.clone();
         let eppr = ExternalProgramProblemRecipe { path, args };
         let inner = track!(eppr.create_factory(registry))?;
 
-        Ok(EmbeddedScriptProblemFactory { inner, temp })
+        Ok(EmbeddedScriptProblemFactory { inner })
     }
 }
 
@@ -53,7 +71,6 @@ impl ProblemRecipe for EmbeddedScriptProblemRecipe {
 #[derive(Debug)]
 pub struct EmbeddedScriptProblemFactory {
     inner: ExternalProgramProblemFactory,
-    temp: TempPath,
 }
 impl ProblemFactory for EmbeddedScriptProblemFactory {
     type Problem = EmbeddedScriptProblem;
